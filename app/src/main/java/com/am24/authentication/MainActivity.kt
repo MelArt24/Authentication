12682c1642e29c96432222907d8739111d59cd4e
MelArt24 <animator2404@gmail.com>
2025-06-18 16:16:14 +0300

    Revert "Update .gitignore"
    This reverts commit 10d4d086fd97b57ce79157c367837010619e513c.

--- app/src/main/java/com/am24/authentication/MainActivity.kt
+++ app/src/main/java/com/am24/authentication/MainActivity.kt
@@ -2,15 +2,10 @@ package com.am24.authentication
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.am24.authentication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
     }
 }
