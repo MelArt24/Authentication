aeed81289f85bcb0d676644893b6207423343d3d
MelArt24 <animator2404@gmail.com>
2025-06-19 12:15:31 +0300

    Update LoginActivity and SignUpActivity

--- app/src/main/java/com/am24/authentication/LoginActivity.kt
+++ app/src/main/java/com/am24/authentication/LoginActivity.kt
@@ -2,10 +2,16 @@ package com.am24.authentication
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.am24.authentication.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.title = "Login"
     }
 }
--- app/src/main/java/com/am24/authentication/SignUpActivity.kt
+++ app/src/main/java/com/am24/authentication/SignUpActivity.kt
@@ -2,10 +2,16 @@ package com.am24.authentication
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.am24.authentication.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignUpBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.title = "Sign Up"
     }
 }
