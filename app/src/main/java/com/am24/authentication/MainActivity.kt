10288d094e199cdc1940d9e3e39ef48bc6756c36
MelArt24 <animator2404@gmail.com>
2025-06-19 12:22:08 +0300

    Update MainActivity

--- app/src/main/java/com/am24/authentication/MainActivity.kt
+++ app/src/main/java/com/am24/authentication/MainActivity.kt
@@ -1,16 +1,42 @@
 package com.am24.authentication
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.am24.authentication.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    companion object {
+        lateinit var authantication: FirebaseAuth
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        authantication = FirebaseAuth.getInstance()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.signIn.setOnClickListener {
+            startActivity(Intent(this, SignUpActivity::class.java))
+            finish()
+        }
+
+        binding.signOut.setOnClickListener {
+            authantication.signOut()
+            binding.user.text = updateData()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.user.text = updateData()
+    }
+
+    private fun updateData(): String {
+        return "Welcome, ${authantication.currentUser?.email}"
     }
 }
