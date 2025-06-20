7879a04567c32e4e38985a5f6af3438f5ba9b999
MelArt24 <animator2404@gmail.com>
2025-06-20 13:43:32 +0300

    Make some changes - update Kotlin library from 1.9.24 to 2.1.0 - update SignUpActivity - update LoginActivity

--- app/src/main/java/com/am24/authentication/LoginActivity.kt
+++ app/src/main/java/com/am24/authentication/LoginActivity.kt
@@ -1,6 +1,8 @@
 package com.am24.authentication
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.am24.authentication.databinding.ActivityLoginBinding
 
@@ -13,5 +15,25 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
         supportActionBar?.title = "Sign Up"
+
+        binding.signupTV.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }
+
+        binding.loginBtn.setOnClickListener {
+            val email = binding.emailLogin.text.toString()
+            val password = binding.passwordLogin.text.toString()
+
+            if(email.isNotEmpty() && password.isNotEmpty()) {
+                MainActivity.authantication.signInWithEmailAndPassword(email, password).addOnCompleteListener {
+                    if(it.isSuccessful) {
+                        startActivity(Intent(this, MainActivity::class.java))
+                    }
+                }.addOnFailureListener {
+                    Toast.makeText(this, it.localizedMessage, Toast.LENGTH_LONG).show()
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/am24/authentication/SignUpActivity.kt
+++ app/src/main/java/com/am24/authentication/SignUpActivity.kt
@@ -2,6 +2,7 @@ package com.am24.authentication
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.am24.authentication.databinding.ActivitySignUpBinding
 
@@ -18,6 +19,21 @@ class SignUpActivity : AppCompatActivity() {
         binding.loginTV.setOnClickListener {
             startActivity(Intent(this, LoginActivity::class.java))
             finish()
+        }
+
+        binding.createAccountBtn.setOnClickListener {
+            val email = binding.emailSignUp.text.toString()
+            val password = binding.passwordSignUp.text.toString()
+
+            if(email.isNotEmpty() && password.isNotEmpty()) {
+                MainActivity.authantication.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
+                    if(it.isSuccessful) {
+                        startActivity(Intent(this, LoginActivity::class.java))
+                    }
+                }.addOnFailureListener {
+                    Toast.makeText(this, it.localizedMessage, Toast.LENGTH_LONG).show()
+                }
+            }
         }
     }
 }
